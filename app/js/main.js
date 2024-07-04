da39a9c914e0d69c04986775c7f0461261b08c4b
Christina <150205323+berryalien@users.noreply.github.com>
2024-07-04 22:11:42 +0400

    made a slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
 $(function () {
 
-$('.article-slider').slick({
+$('.article-slider__box').slick({
+  dots: true,
 prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/feedback/arrow-prev.svg" alt="arrow left"></button>',
 nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/feedback/arrow-next.svg" alt="arrow right"></button>'
 });
